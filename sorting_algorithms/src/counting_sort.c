2f736feaae6944b4b0b475b4d05e25cc2208e45c
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2025-01-20 16:27:55 +0100

    counting sort bla ma nsyih pii

---
+++ sorting_algorithms/src/counting_sort.c
@@ -0,0 +1,41 @@
+#include "../include/counting_sort.h"
+#include <stdlib.h>
+
+void counting_sort(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    int* count = (int*)calloc(max + 1, sizeof(int));
+    if (count == NULL) {
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    for (int i = 1; i <= max; i++) {
+        count[i] += count[i - 1];
+    }
+
+    int* output = (int*)malloc(n * sizeof(int));
+    if (output == NULL) {
+        free(count);
+        return;
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+
+    free(count);
+    free(output);
+}
