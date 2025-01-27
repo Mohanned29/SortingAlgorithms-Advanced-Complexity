190d684cf1e8191b91d5ad3f7fe0a82cee4e1fb5
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2025-01-27 16:24:05 +0100

    almost done with it , adding all the sorting algorithms

--- sorting_algorithms/src/main.c
+++ sorting_algorithms/src/main.c
@@ -26,6 +26,7 @@
 
 #define MAX_VALUE 100
 #define DELAY_MILLISECONDS 100
+
 void clear_screen() {
     system("cls");
 }
@@ -73,6 +74,74 @@ void bubble_sort_visual(int arr[], int n) {
     print_array_colored(arr, n, -1, -1);
 }
 
+void gnome_sort_visual(int arr[], int n) {
+    int i = 0;
+    while (i < n) {
+        clear_screen();
+        printf("%sGnome Sort - Checking position %d%s\n", YELLOW, i, RESET);
+        print_array_colored(arr, n, i, i > 0 ? i - 1 : -1);
+        create_delay();
+
+        if (i == 0 || arr[i - 1] <= arr[i]) {
+            i++;
+        } else {
+            int temp = arr[i];
+            arr[i] = arr[i - 1];
+            arr[i - 1] = temp;
+            i--;
+        }
+    }
+    clear_screen();
+    printf("%sGnome Sort - Final Result:%s\n", GREEN, RESET);
+    print_array_colored(arr, n, -1, -1);
+}
+
+void heapify_visual(int arr[], int n, int i, int total_size) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    clear_screen();
+    printf("%sHeap Sort - Heapifying around index %d%s\n", YELLOW, i, RESET);
+    print_array_colored(arr, total_size, left < n ? left : -1, right < n ? right : -1);
+    create_delay();
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        int swap = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = swap;
+        heapify_visual(arr, n, largest, total_size);
+    }
+}
+
+void heap_sort_visual(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify_visual(arr, n, i, n);
+
+    for (int i = n - 1; i > 0; i--) {
+        clear_screen();
+        printf("%sHeap Sort - Extracting maximum%s\n", YELLOW, RESET);
+        print_array_colored(arr, n, 0, i);
+        create_delay();
+
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify_visual(arr, i, 0, n);
+    }
+    
+    clear_screen();
+    printf("%sHeap Sort - Final Result:%s\n", GREEN, RESET);
+    print_array_colored(arr, n, -1, -1);
+}
+
 void quick_sort_visual_recursive(int arr[], int low, int high, int n) {
     if (low < high) {
         clear_screen();
@@ -90,6 +159,107 @@ void quick_sort_visual(int arr[], int n) {
     quick_sort_visual_recursive(arr, 0, n - 1, n);
     clear_screen();
     printf("%sQuick Sort - Final Result:%s\n", GREEN, RESET);
+    print_array_colored(arr, n, -1, -1);
+}
+
+void counting_sort_visual(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    int* count = (int*)calloc(max + 1, sizeof(int));
+    if (count == NULL) return;
+
+    for (int i = 0; i < n; i++) {
+        clear_screen();
+        printf("%sCounting Sort - Counting frequency of %d%s\n", YELLOW, arr[i], RESET);
+        print_array_colored(arr, n, i, -1);
+        create_delay();
+        count[arr[i]]++;
+    }
+
+    for (int i = 1; i <= max; i++) {
+        count[i] += count[i - 1];
+    }
+
+    int* output = (int*)malloc(n * sizeof(int));
+    if (output == NULL) {
+        free(count);
+        return;
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        clear_screen();
+        printf("%sCounting Sort - Placing element %d in correct position%s\n", YELLOW, arr[i], RESET);
+        print_array_colored(arr, n, i, -1);
+        create_delay();
+        
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+
+    free(count);
+    free(output);
+    
+    clear_screen();
+    printf("%sCounting Sort - Final Result:%s\n", GREEN, RESET);
+    print_array_colored(arr, n, -1, -1);
+}
+
+void counting_sort_for_radix_visual(int arr[], int n, int exp) {
+    int* output = (int*)malloc(n * sizeof(int));
+    int count[10] = {0};
+    
+    for (int i = 0; i < n; i++) {
+        clear_screen();
+        printf("%sRadix Sort - Counting digit %d%s\n", YELLOW, (arr[i] / exp) % 10, RESET);
+        print_array_colored(arr, n, i, -1);
+        create_delay();
+        count[(arr[i] / exp) % 10]++;
+    }
+
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+
+    free(output);
+}
+
+void radix_sort_visual(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        clear_screen();
+        printf("%sRadix Sort - Sorting by digit position %d%s\n", YELLOW, exp, RESET);
+        print_array_colored(arr, n, -1, -1);
+        create_delay();
+        
+        counting_sort_for_radix_visual(arr, n, exp);
+    }
+    
+    clear_screen();
+    printf("%sRadix Sort - Final Result:%s\n", GREEN, RESET);
     print_array_colored(arr, n, -1, -1);
 }
 
@@ -128,10 +298,14 @@ int main() {
         printf("\n%sChoose a sorting algorithm:%s\n", MAGENTA, RESET);
         printf("1. Bubble Sort\n");
         printf("2. Quick Sort\n");
-        printf("3. Exit\n");
+        printf("3. Heap Sort\n");
+        printf("4. Counting Sort\n");
+        printf("5. Radix Sort\n");
+        printf("6. Gnome Sort\n");
+        printf("7. Exit\n");
         
         int choice;
-        printf("\nEnter your choice (1-3): ");
+        printf("\nEnter your choice (1-7): ");
         scanf("%d", &choice);
         
         int* arr_copy = (int*)malloc(n * sizeof(int));
@@ -140,6 +314,7 @@ int main() {
             free(arr);
             return 1;
         }
+        
         memcpy(arr_copy, arr, n * sizeof(int));
         
         switch (choice) {
@@ -150,6 +325,18 @@ int main() {
                 quick_sort_visual(arr_copy, n);
                 break;
             case 3:
+                heap_sort_visual(arr_copy, n);
+                break;
+            case 4:
+                counting_sort_visual(arr_copy, n);
+                break;
+            case 5:
+                radix_sort_visual(arr_copy, n);
+                break;
+            case 6:
+                gnome_sort_visual(arr_copy, n);
+                break;
+            case 7:
                 free(arr);
                 free(arr_copy);
                 printf("\n%sThank you for using the Sorting Algorithm Visualizer!%s\n", GREEN, RESET);
@@ -168,3 +355,5 @@ int main() {
     
     return 0;
 }
+
+// gcc -o sort_benchmark main.c bubble_sort.c counting_sort.c gnome_sort.c heap_sort.c quick_sort.c radix_sort.c utils.c -I../include
