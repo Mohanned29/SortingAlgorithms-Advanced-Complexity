109b4abcba99f853717da765147940e9c9858b32
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2025-01-27 16:01:26 +0100

    fixing stuff

--- sorting_algorithms/include/radix_sort.h
+++ sorting_algorithms/include/radix_sort.h
@@ -1,8 +1,6 @@
 #ifndef RADIX_SORT_H
 #define RADIX_SORT_H
 
-int key(int x, int i);
-void sort_aux(int arr[], int n, int i);
-void radix_sort(int arr[], int n, int k);
+void radix_sort(int arr[], int n);
 
 #endif
--- sorting_algorithms/src/main.c
+++ sorting_algorithms/src/main.c
@@ -6,53 +6,91 @@
 #include "../include/radix_sort.h"
 #include "../include/quick_sort.h"
 #include "../include/heap_sort.h"
+#include "../include/counting_sort.h"
 #include "../include/utils.h"
+
 
 #define MAX_SIZE 1000000
 #define MAX_VALUE 1000
 #define NUM_TESTS 5
 
-int main() {
-    int sizes[] = {100, 1000, 5000, 10000, 50000, 100000, 500000, 1000000};
-    int num_sizes = sizeof(sizes) / sizeof(sizes[0]);
+typedef void (*SortFunction)(int[], int);
+
+typedef struct {
+    const char* name;
+    SortFunction function;
+} SortAlgorithm;
+
+void run_sorting_benchmark(const SortAlgorithm algorithms[], int num_algorithms, 
+                         const int sizes[], int num_sizes) {
+    int* arr = (int*)malloc(MAX_SIZE * sizeof(int));
+    int* arr_copy = (int*)malloc(MAX_SIZE * sizeof(int));
+    
+    if (!arr || !arr_copy) {
+        fprintf(stderr, "Memory allocation failed\n");
+        free(arr);
+        free(arr_copy);
+        return;
+    }
 
     for (int i = 0; i < num_sizes; i++) {
         int n = sizes[i];
-        int arr[MAX_SIZE];
-        double total_times[5] = {0};
+        double* total_times = (double*)calloc(num_algorithms, sizeof(double));
+        
+        if (!total_times) {
+            fprintf(stderr, "Memory allocation failed for times array\n");
+            continue;
+        }
 
-        printf("Array size: %d\n", n);
+        printf("\nArray size: %d\n", n);
+        printf("----------------------------------------\n");
 
         for (int test = 0; test < NUM_TESTS; test++) {
             generate_random_array(arr, n, MAX_VALUE);
 
-            int arr_copy[MAX_SIZE];
-            for (int j = 0; j < n; j++) arr_copy[j] = arr[j];
+            for (int algo = 0; algo < num_algorithms; algo++) {
+                for (int j = 0; j < n; j++) {
+                    arr_copy[j] = arr[j];
+                }
 
-            total_times[0] += measure_time(bubble_sort, arr_copy, n);
-            for (int j = 0; j < n; j++) arr_copy[j] = arr[j];
-
-            total_times[1] += measure_time(bubble_sort_optimized, arr_copy, n);
-            for (int j = 0; j < n; j++) arr_copy[j] = arr[j];
-
-            total_times[2] += measure_time(gnome_sort, arr_copy, n);
-            for (int j = 0; j < n; j++) arr_copy[j] = arr[j];
-
-            total_times[3] += measure_time(quick_sort, arr_copy, n);
-            for (int j = 0; j < n; j++) arr_copy[j] = arr[j];
-
-            total_times[4] += measure_time(heap_sort, arr_copy, n);
+                total_times[algo] += measure_time(algorithms[algo].function, arr_copy, n);
+            }
         }
 
         printf("Average times over %d tests:\n", NUM_TESTS);
-        printf("Bubble Sort: %.6f seconds\n", total_times[0] / NUM_TESTS);
-        printf("Optimized Bubble Sort: %.6f seconds\n", total_times[1] / NUM_TESTS);
-        printf("Gnome Sort: %.6f seconds\n", total_times[2] / NUM_TESTS);
-        printf("Quick Sort: %.6f seconds\n", total_times[3] / NUM_TESTS);
-        printf("Heap Sort: %.6f seconds\n", total_times[4] / NUM_TESTS);
-
+        for (int algo = 0; algo < num_algorithms; algo++) {
+            printf("%-20s: %.6f seconds\n", 
+                   algorithms[algo].name, 
+                   total_times[algo] / NUM_TESTS);
+        }
         printf("\n");
+
+        free(total_times);
     }
+
+    free(arr);
+    free(arr_copy);
+}
+
+int main() {
+    const SortAlgorithm algorithms[] = {
+        {"Bubble Sort", bubble_sort},
+        {"Optimized Bubble", bubble_sort_optimized},
+        {"Gnome Sort", gnome_sort},
+        {"Quick Sort", quick_sort},
+        {"Heap Sort", heap_sort},
+        {"Counting Sort", counting_sort},
+        {"Radix Sort", radix_sort}
+    };
+    const int num_algorithms = sizeof(algorithms) / sizeof(algorithms[0]);
+
+    const int sizes[] = {100, 1000, 5000, 10000, 50000, 100000, 500000, 1000000};
+    const int num_sizes = sizeof(sizes) / sizeof(sizes[0]);
+
+    printf("Sorting Algorithm Benchmark\n");
+    printf("=============================\n");
+    
+    run_sorting_benchmark(algorithms, num_algorithms, sizes, num_sizes);
 
     return 0;
 }
